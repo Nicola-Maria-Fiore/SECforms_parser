6e53f844a5507dc0636a0c1e7c2b31492459fdaa
Flower Power <ninnofiore@outlook.com>
2021-05-15 16:40:02 +0200

    Add files via upload

---
+++ sql.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="123456",
+  database="db"
+)
+mycursor = mydb.cursor()
+
+sql="SHOW TABLES;"
+
+mycursor.execute(sql)
+myresult = mycursor.fetchall()
+for x in myresult:
+  print(x)
